e81c06ea653fa5201ada9477de49aeea5289bc95
mina-gwak <mina.gwak@kakao.com>
2022-05-27 18:08:39 +0900

    feat: 첫번째 메인 컨텐츠 구현 및 두번째 컨텐츠와 병합 (CODESQUAD-AIRBNB/airbnb#12)

--- FE/src/components/MainContent/index.tsx
+++ FE/src/components/MainContent/index.tsx
@@ -1,16 +1,13 @@
-import { MainCategory } from '@components/MainContent/MainCategory';
+import CitySection from '@components/MainContent/CitySection';
 import * as S from '@components/MainContent/MainContent.style';
-import { mainContentData, MainContentDataType } from '@components/MainContent/constants';
+import TripSection from '@components/MainContent/TripSection';
+
 const MainContent = () => {
   return (
     <S.Container>
       <S.Wrapper>
-        <S.MainContentTitle>어디서나, 여행은 살아보는 거야!</S.MainContentTitle>
-        <S.MainCategoryWrapper>
-          {mainContentData.map((categoryData: MainContentDataType) => (
-            <MainCategory categoryData={categoryData} />
-          ))}
-        </S.MainCategoryWrapper>
+        <CitySection />
+        <TripSection />
       </S.Wrapper>
     </S.Container>
   );
