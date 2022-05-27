7df9523f5d6bd5a62f3bc607e28a12dcff9b9802
mina-gwak <mina.gwak@kakao.com>
2022-05-27 12:39:10 +0900

    feat: SearchBar 데이터, 상수 추가 (CODESQUAD-AIRBNB/airbnb#12)

--- FE/src/data/index.js
+++ FE/src/data/index.js
@@ -1,1 +1,10 @@
 export { nav, NavTypes } from '@data/nav';
+export {
+  searchBarData,
+  defaultPrice,
+  defaultPeriod,
+  defaultPersonnel,
+  PersonnelTypes,
+  PriceTypes,
+  PeriodTypes,
+} from '@data/searchBarData';
